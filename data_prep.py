e4fe3ab55a5a41eeff6b8a4a3f85bc291a9747df
PhOeNiX <benhassineyoussef04@gmail.com>
2025-04-15 00:09:23 +0200

    correction des intervalles des outliers

--- data_prep.py
+++ data_prep.py
@@ -20,7 +20,7 @@ def preprocess_data():
         'Unspecified': 'United Kingdom',
         'RSA': 'South Africa',
     })
-    df = df[(df['UnitPrice'] > 0) & (df['UnitPrice'] <= 200)]
+    df = df[(df['UnitPrice'] > 0) & (df['UnitPrice'] <= 100)]
     df = df[(df['Quantity'] > 0) & (df['Quantity'] <= 1500)]
     df = df[(df['TotalPrice'] > 0) & (df['TotalPrice'] <= 7000)]
     df = df[(df['InvoiceDate'] >= '2010-12-01') & (df['InvoiceDate'] <= '2011-12-09')]
